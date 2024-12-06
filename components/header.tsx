5e72d170a90cd0241b5fa72a7b372a67e8e88008
sahapranta <pranta1204@gmail.com>
2024-12-07 01:35:51 +0600

    image is not showing

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,7 @@ export const Header: React.FC = async () => {
     <header className="sticky py-1 md:py-2 px-4 flex justify-between items-center z-10 backdrop-filter backdrop-blur-lg shadow-md bg-gray-100/80 dark:bg-gray-400/60 border-b border-gray-200 dark:border-gray-400 md:mx-12 mx-4 mt-2 rounded-3xl">
       <div>
         <a href="/" className='flex items-center gap-1'>
-          <img src="http://localhost:3000/images/logo.png" alt="AIAPP Logo" className={cn('h-7')} />
+          <img src="/images/logo.png" alt="AIAPP Logo" className={cn('h-7')} />
           <span className="text-black dark:text-gray-300 font-extrabold text-lg">AIAPP. <span className='text-orange-800/70 dark:text-orange-400'>GG</span></span>
         </a>
       </div>
